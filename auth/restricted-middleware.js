6c472d6fe538049a6ae66211a65b161390d5aa86
dragonbegin <juan.reyes.mba@gmail.com>
2019-09-05 19:10:14 -0500

    first commit

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,12 +3,15 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
+  // this shouldn't happen
+  //grabbing a cookie
   const { username, password } = req.headers;
-
+  //make sure cookie is valid
   if (username && password) {
     Users.findBy({ username })
       .first()
       .then(user => {
+        //we never need to check the password
         if (user && bcrypt.compareSync(password, user.password)) {
           next();
         } else {
